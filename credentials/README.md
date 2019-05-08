70afbac8ab5e1fd8ed273820181ed00c2051d6cb
Eugenio Streliaev <geninhofloripa@hotmail.com>
2019-05-08 08:33:33 -0300

    adjustments only

---
+++ credentials/README.md
@@ -0,0 +1,10 @@
+# Credentials format
+
+## Algorithmia
+
+File: `algorithmia.json`
+
+```{
+    "apiKey": "simbNlAJHojoC3u/Xi3OKAvTDbU1"
+}
+```
--- robots/text.js
+++ robots/text.js
@@ -7,32 +7,24 @@ async function robot(content) {
     sanitizeContent(content)
     breakContentIntoSentences(content)
 
-    async function fetchContentFromWikipedia(content) {
-        //console.log('> [text-robot] Fetching content from Wikipedia')
 
+    async function fetchContentFromWikipedia(content) {
         const algorithmiaAuthenticated = algorithmia(algorithmiaApiKey)
         const wikipediaAlgorithm = algorithmiaAuthenticated.algo('web/WikipediaParser/0.1.2')
         const wikipediaResponse = await wikipediaAlgorithm.pipe(content.searchTerm)
         const wikipediaContent = wikipediaResponse.get()
 
         content.sourceContentOriginal = wikipediaContent.content
-        //console.log(wikipediaContent.content)
-        //console.log('> [text-robot] Fetching done!')
-
     }
 
     function sanitizeContent(content) {
         const withoutBlankLinesAndMarkdown = removeBlankLinesAndMarkdown(content.sourceContentOriginal)
         const withoutDatesInParenthesis = removeDatesInParenthesis(withoutBlankLinesAndMarkdown)
-        //console.log(withoutBlankLinesAndMarkdown)
-        //console.log(withoutDatesInParenthesis)
 
         content.sourceContentSanitized = withoutDatesInParenthesis
 
         function removeBlankLinesAndMarkdown(text) {
             const allLines = text.split('\n')
-            //console.log(allLines)
-
             const withoutBlankLinesAndMarkdown = allLines.filter((line) => {
                 if(line.trim().length === 0 || line.trim().startsWith('=')) {
                     return false
